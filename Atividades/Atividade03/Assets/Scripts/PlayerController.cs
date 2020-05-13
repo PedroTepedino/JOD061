20e74173435a14632ad474e480751eb5ed200740
Pedro Tepedino <pedrotepedinossauro@gmail.com>
2020-05-13 17:19:42 -0300

    Criando o projeto Atividade03

---
+++ Atividades/Atividade03/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour 
+{
+    public float moveSpeed = 0.2f;
+    public float moveRotation = 20f;
+
+    private void Update() 
+    {
+        if (isLocalPlayer)
+        {
+            transform.Translate(0, 0, Input.GetAxis("Vertical") * moveSpeed);
+            transform.Rotate(0, Input.GetAxis("Horizontal") * moveRotation, 0);
+        }    
+    }
+}
+
