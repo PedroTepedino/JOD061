c99308c680973589be2f367dc923ca271b79b383
Pedro Tepedino <pedrotepedinossauro@gmail.com>
2020-05-23 19:49:38 -0300

    Atividade04

---
+++ Atividades/Atividade04/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using Mirror;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PlayerController : NetworkBehaviour
+{
+    [SerializeField] private float _moveSpeed = 0.1f;
+    [SerializeField] private float _moveRotation = 1f;
+    [SerializeField] private GameObject _projectilePrefab;
+    [SerializeField] private Transform _projectilePosition;
+
+    private void Start()
+    {
+        Material material = GetComponent<Renderer>().material;
+        material.color = new Color(Random.Range(0f, 1f),Random.Range(0f, 1f),Random.Range(0f, 1f));
+    }
+
+    private void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+        
+        transform.Translate(0, 0, Input.GetAxis("Vertical") * _moveSpeed);
+        transform.Rotate(0, Input.GetAxis("Horizontal") * _moveRotation, 0f);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            CmdShoot();
+    }
+
+    [Command]
+    private void CmdShoot()
+    {
+        GameObject projectile = Instantiate(_projectilePrefab, _projectilePosition.position, this.transform.rotation);
+        NetworkServer.Spawn(projectile);
+    }
+}
---
+++ Atividades/Atividade04/Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Mirror;
+using UnityEngine;
+
+public class ProjectileController : NetworkBehaviour
+{
+    [SerializeField] private float _moveSpeed = 20f;
+    
+    private void Start()
+    {
+        Destroy(this.gameObject, 1f);
+    }
+
+    private void Update()
+    {
+        transform.Translate(0, 0, Time.deltaTime * _moveSpeed);
+    }
+
+    [ServerCallback]
+    private void OnTriggerEnter(Collider other)
+    {
+        NetworkServer.Destroy(gameObject);
+        NetworkServer.Destroy(other.gameObject);
+    }
+}
